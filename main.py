441478758caba5effc05f92ddae68a6f41945ad0
Hugo Eduardo Ziviani <hugo_ziviani@hotmail.com>
2020-07-16 21:03:44 -0300

    cleaning code and add doc strings

--- main.py
+++ main.py
@@ -3,73 +3,81 @@ import json
 
 
 class ProcessProjects:
-    def __init__(self, waypath):
-        self.wayPath = waypath
+    """
+    ProcessProjects class implemented the main functionality of this tinny project.
+    The json file is recived and the managers and watchers are separeteds
+    After we have got the list of managers and watchers from the projects    
+    the whole data is performed to find the each project whitch is anaged 
+    or watched the previous list gotten.
+    """
+
+    def __init__(self, path):
+        self.path = path
         self.projects = None
         self.managers = []
         self.watchers = []
-        self.watchersAndProjects = {}
-        self.managersAndProjects = {}
+        self.watchers_and_rojects = {}
+        self.managers_and_rojects = {}
 
-    def __getJsonData(self):
-        jsonFile = pd.read_json(self.wayPath)
-        self.projects = jsonFile.sort_values(by=['priority'])
+    def __get_json_data(self):
+        json_file = pd.read_json(self.path)
+        self.projects = json_file.sort_values(by=['priority'])
 
-    def __saveManagers(self):
-        data = json.dumps(self.managersAndProjects)
-        with open('/home/hz/Documents/deepHugo/deppHugo/outputs/managers.json', 'w') as outfile:
+    def __save_managers(self):
+        data = json.dumps(self.managers_and_rojects)
+        with open('outputs/managers.json', 'w') as outfile:
             json.dump(data, outfile, indent=4)
 
-    def __saveWatchers(self):
-        data = json.dumps(self.watchersAndProjects)
-        with open('/home/hz/Documents/deepHugo/deppHugo/outputs/watchers.json', 'w') as outfile:
+    def __save_watchers(self):
+        data = json.dumps(self.watchers_and_rojects)
+        with open('outputs/watchers.json', 'w') as outfile:
             json.dump(data, outfile, indent=4)
 
-    def projectsETL(self):
-        managersColumn = self.projects["managers"]
-        watchersColumn = self.projects["watchers"]
+    def projects_etl(self):
+        managers_column = self.projects["managers"]
+        watchers_column = self.projects["watchers"]
 
-        for managerList in managersColumn:
-            for manager in managerList:
+        for manager_list in managers_column:
+            for manager in manager_list:
                 if manager not in self.managers:
                     self.managers.append(manager)
 
-        for watcherList in watchersColumn:
-            for watcher in watcherList:
+        for watcher_list in watchers_column:
+            for watcher in watcher_list:
                 if watcher not in self.watchers:
                     self.watchers.append(watcher)
 
         df = self.projects
-        finaManagerlList = {}
-        for managerName in self.managers:
-            name = managerName
+        final_manager_list = {}
+        for manager_name in self.managers:
+            name = manager_name
             projects = []
             for row in df.index:
-                if managerName in df.iloc[row]["managers"]:
-                    projectName = df.iloc[row]["name"]
-                    projects.append(projectName)
-            finaManagerlList[name] = projects
+                if manager_name in df.iloc[row]["managers"]:
+                    project_name = df.iloc[row]["name"]
+                    projects.append(project_name)
+            final_manager_list[name] = projects
         
-        self.managersAndProjects = finaManagerlList
+        self.managers_and_rojects = final_manager_list
         
-        finaWatcherlList = {}
+        final_watcher_list = {}
         for watcher in self.watchers:
             name = watcher
             projects = []
             for row in df.index:
                 if watcher in df.iloc[row]["watchers"]:
-                    projectName = df.iloc[row]["name"]
-                    projects.append(projectName)
-            finaWatcherlList[name] = projects
+                    project_name = df.iloc[row]["name"]
+                    projects.append(project_name)
+            final_watcher_list[name] = projects
         
-        self.watchersAndProjects = finaWatcherlList
+        self.watchers_and_rojects = final_watcher_list
 
-    def prepareObject(self):
-        self.__getJsonData()
-        self.projectsETL()
-        self.__saveManagers()
-        self.__saveWatchers()
+    def prepare_object(self):
+        self.__get_json_data()
+        self.projects_etl()
+        self.__save_managers()
+        self.__save_watchers()
 
 
-projects = ProcessProjects("source_file.json")
-projects.prepareObject()
+projects = ProcessProjects("inputs/source_file.json")
+projects.prepare_object()
